978a33c64890e708c6e63d09c9f9d62ca6751dc3
Solarcow <Alex.Kamrath@live.com>
2016-04-13 11:11:43 +1000

    move format classes into module and add gems

--- lib/listable.rb
+++ lib/listable.rb
@@ -3,4 +3,14 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+  def format_name(site_name)
+    site_name ? "#{site_name}" : ""
+  end
+  def format_priority(priority)
+    value = " ⇧".colorize(:red) if priority == "high"
+    value = " ⇨".colorize(:yellow) if priority == "medium"
+    value = " ⇩".colorize(:green) if priority == "low"
+    value = "" if !priority
+    return value
+ end
 end
