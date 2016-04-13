4480aac20e082bcdbccbbfd6d4e26ee3e82b4db5
Solarcow <Alex.Kamrath@live.com>
2016-04-13 11:46:56 +1000

    add error handling for delete and priority

--- lib/listable.rb
+++ lib/listable.rb
@@ -6,11 +6,28 @@ module Listable
   def format_name(site_name)
     site_name ? "#{site_name}" : ""
   end
-  def format_priority(priority)
-    value = " ⇧".colorize(:red) if priority == "high"
-    value = " ⇨".colorize(:yellow) if priority == "medium"
-    value = " ⇩".colorize(:green) if priority == "low"
-    value = "" if !priority
-    return value
+  # def format_priority(priority)
+  #   value = " ⇧".colorize(:red) if priority == "high"
+  #   value = " ⇨".colorize(:yellow) if priority == "medium"
+  #   value = " ⇩".colorize(:green) if priority == "low"
+  #   value = "" if !priority
+  #   return value
+  # end
+ def format_priority(priority)
+  	if priority == "high"
+	    value = " ⇧".colorize(:red)
+	    return value
+	elsif priority == "medium"
+	    value = " ⇨".colorize(:yellow)
+	    return value
+	elsif priority == "low"
+	    value = " ⇩".colorize(:green)
+	    return value
+	elsif !priority
+		value = ""
+		return value
+	else
+	    raise UdaciListErrors::InvalidPriorityValue, "#{priority} is not a valid priority"
+	end
  end
 end
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -2,17 +2,27 @@ class UdaciList
   attr_reader :title, :items
 
   def initialize(options={})
-    @title = "Untitled List" || options[:title]
+    @title = options[:title] || "Untitled List"
     @items = []
   end
   def add(type, description, options={})
     type = type.downcase
-    @items.push TodoItem.new(description, options) if type == "todo"
-    @items.push EventItem.new(description, options) if type == "event"
-    @items.push LinkItem.new(description, options) if type == "link"
+    if type == "todo"
+      @items.push TodoItem.new(description, options)
+    elsif type == "event"
+      @items.push EventItem.new(description, options)
+    elsif type == "link"
+      @items.push LinkItem.new(description, options)
+    else
+      raise UdaciListErrors::InvalidItemType, "#{type} is not a valid item type"
+    end
   end
   def delete(index)
-    @items.delete_at(index - 1)
+    if index > @items.length
+      raise UdaciListErrors::IndexExceedsListSize, "item #{index} does not exist"
+    else
+      @items.delete_at(index - 1)
+    end
   end
   def all
     puts "-" * @title.length
